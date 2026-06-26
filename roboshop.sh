b428522fa8c5ce7d2f13a6755188532d6b9ab4e5
Prashanth12P <pprashanth2812@gmail.com>
2026-06-26 17:35:47 +0100

    function to add instances to to AWS via script

---
+++ roboshop.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+securityid ="sg-084020e387d413aac"
+AmiId ="ami-0220d79f3f480ecf5"
+
+for Instance in $@
+do
+instanceid=$(aws ec2 run-instances \
+    --image-id $AmiId \
+    --count 1 \
+    --instance-type t3.micro \
+    --security-group-ids $securityid \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=$Instance,Value=MyServer}]' \
+	--query "Instances[0].InstanceId" \
+    --output text)
+if [ $Instance = 'Frontend' ]
+then
+IP=$(aws ec2 describe-instances \
+    --instance-ids $instanceid \
+    --query "Reservations[*].Instances[*].PublicIpAddress" \
+    --output text
+)
+else
+IP=(aws ec2 describe-instances \
+    --instance-ids $instanceid \
+    --query "Reservations[*].Instances[*].PrivateIpAddress" \
+    --output text
+)
+fi
+done 

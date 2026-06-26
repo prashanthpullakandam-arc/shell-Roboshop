63021c37746b7bf7dae6dd2e1ac742b5f0926ba2
Prashanth12P <pprashanth2812@gmail.com>
2026-06-26 18:24:43 +0100

    function to add a record

--- roboshop.sh
+++ roboshop.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 securityid="sg-084020e387d413aac"
 AmiId="ami-0220d79f3f480ecf5"
+Hostzone="Z0529426UUSGOVH6OLOK"
+DomainName="ananthsolutions.online"
 
 for Instance in $@
 do
@@ -20,11 +22,31 @@ IP=$(aws ec2 describe-instances \
     --query "Reservations[*].Instances[*].PublicIpAddress" \
     --output text)
     echo "$IP"
+    RecordName=$DomainName
+    
 else
 IP=$(aws ec2 describe-instances \
     --instance-ids $instanceid \
     --query "Reservations[*].Instances[*].PrivateIpAddress" \
     --output text)
     echo "$IP"
+   RecordName="$Instance.$DomainName"
+
 fi
+aws route53 change-resource-record-sets \
+  --hosted-zone-id $Hostzone \
+  --change-batch '{
+    "Changes": [
+      {
+        "Action": "CREATE",
+        "ResourceRecordSet": {
+          "Name": "'$RecordName'",
+          "Type": "A",
+          "TTL": 300,
+          "ResourceRecords": [{"Value": "'$IP'"}]
+        }
+      }
+    ]
+  }'
 done 
+ 

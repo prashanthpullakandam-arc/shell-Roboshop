ec419b9b8230b564ee4fba10d4d909983aa1148d
Prashanth12P <pprashanth2812@gmail.com>
2026-06-27 14:17:17 +0100

    function to add a record

---
+++ redis.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOG_PATH="/var/log/roboshop"
+LOG="$LOG_PATH/$0.log"
+
+user=$(id -u)
+if [ $user -ne 0 ]
+echo  -e "$R not a root user $N"
+exit 1;
+fi
+mkdir -p $LOG_PATH
+validate(){
+if [ $1 -eq 0 ]
+echo -e "$G $2 successful$N" | tee -a $LOG
+else
+echo -e "$R $2 failed $N" | tee -a $LOG
+fi
+}
+dnf module disable redis -y
+dnf module enable redis:7 -y
+validate $? "redis 7 enabled"
+dnf install redis -y
+validate ? "redis installed"
+
+sed -i -e '/s/127.0.0.1/0.0.0.0/' -e '/protected-mode/c\protected-mode no' /etc/redis/redis.conf
+validate ? "edited the redis config"
+systemctl enable redis
+systemctl start redis
+validate ? "redis started"
